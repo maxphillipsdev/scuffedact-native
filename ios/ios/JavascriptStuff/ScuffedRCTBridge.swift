ec2b7b066cca672410c6db21bccd684260e83946
Max Phillips <max@maxphillips.dev>
2023-08-19 16:38:18 +1000

    nice mid point

---
+++ ios/ios/JavascriptStuff/ScuffedRCTBridge.swift
@@ -0,0 +1,12 @@
+//
+//  ScuffedRCTBridge.swift
+//  ios
+//
+//  Created by Max Phillips on 19/8/2023.
+//
+
+import Foundation
+
+class ScuffedRCTBridge: NSObject {
+    
+}
---
+++ ios/ios/JavascriptStuff/ScuffedRCTUIManager.swift
@@ -0,0 +1,8 @@
+//
+//  ScuffedRCTUIManager.swift
+//  ios
+//
+//  Created by Max Phillips on 19/8/2023.
+//
+
+import Foundation
